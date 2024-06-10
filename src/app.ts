2e59c0545cb714ed99bb683b4d49a5768733b0bf
masato-210831 <b86500b@gmail.com>
2024-06-10 15:33:36 +0900

    新しい世代のJavascriptとTypescript

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
-const button = document.querySelector("button")!;
+// const add = (a: number = 1, b:number) => a + b;
 
-function clickHnadler(message: string) {
-  console.log('Clicked' + message)
-}
+// const printOutput: (output: string | number) => void =  output => {
+//   console.log(output)
+// }
 
-if(button) {
-  button.addEventListener('click', clickHnadler.bind(null, "You're welcome"))
-}
+// printOutput(add(b = 2))
+
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
+
+const person = {
+  firstName:'Max',
+  age: 30,
+};
+
+activeHobbies.push(...hobbies);
+
+const add = (...numbers: number[]) => {
+ return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue
+  }, 0)
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
 
 
-button.addEventListener("click", () => {
-  console.log('clicked!')
-});
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+const {firstName, age} = person;
