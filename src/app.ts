d347853154447de3dfe653fdf1ba83402ab60a5a
masato-210831 <b86500b@gmail.com>
2024-06-17 22:28:12 +0900

    Typescript デコレータ終了

--- src/app.ts
+++ src/app.ts
@@ -120,6 +120,87 @@ class Printer {
 
 const p = new Printer();
 
-
 const button = document.querySelector('button')!;
 button.addEventListener('click', p.showMessage);
+
+// ------------------------------------------------
+
+
+interface ValidatorConfig {
+  [prop: string]:{
+    [validatableProp:string]: string[] // ['title', 'positive']
+  }
+}
+
+const registeredValidators: ValidatorConfig = {}; 
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['required'],
+  }
+}
+
+function PositiveNumber (target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['positive'],
+  }
+}
+
+
+function validate(obj: any){
+  const objvalidatiorConfig = registeredValidators[obj.constructor.name]
+
+  if (!objvalidatiorConfig) {
+    return true;
+  }
+
+  let isValid = true;
+  for (const prop in objvalidatiorConfig) {
+    console.log(prop);
+    for (const validator of objvalidatiorConfig[prop]){
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break
+      }
+    }
+  }
+  return isValid;
+}
+
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t:string, p:number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const corseForm = document.querySelector('form')!;
+corseForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price)
+
+  if (!validate(createdCourse)) {
+    alert('正しく入力してください。')
+    return;
+  }
+  console.log(createdCourse)
+})
+
