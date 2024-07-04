ac0deaf9c10e4a3f9a76c8d92ec3f12cd73e93df
masato-210831 <b86500b@gmail.com>
2024-07-04 15:01:27 +0900

    TypeScript セクション13 終了

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,44 @@
+import axios from "axios";
+
 // ここにコードを書きます
+
+
+// api key
+const APIKEY = process.env.GOOGLE_API_KEY;
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+const address = (event: Event) => {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${APIKEY}`
+    )
+    .then((res) => {
+      if (res.data.status !== "OK") {
+        throw new Error("座標を取得できませんでした。");
+      }
+      const coordinates = res.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map')!, {
+        center: coordinates,
+        zoom: 16
+      });
+      new google.maps.Marker({position:coordinates, map:map})
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+};
+
+form.addEventListener("submit", address);
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,43 @@
-const path = require('path');
+const path = require('path') // requireはnode.jsでモジュールをインストールする構文
+const Dotenv =require('dotenv-webpack')
+
 
 module.exports = {
   mode: 'development',
-  entry: './src/app.ts',
+  entry: './src/app.ts', // ブラウザで初めに読み込まれるモジュールを指定、ルートノード？？
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist',
+    filename: 'bundle.js', // 任意の名前でOK!!
+    path: path.resolve(__dirname, 'dist'), // 絶対pathなので注意！！
+    publicPath: '/dist' // dev-serverからの相対パス、紐付け
   },
   devtool: 'inline-source-map',
+  devServer: {
+    static:[
+      {
+        directory: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist',
+      },
+      {
+        directory: __dirname,
+        publicPath: '/',
+      },
+    ],
+  },
+  devtool: 'eval',
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js'] // import文からtsとjs拡張子のファイルを探す
   },
+  plugins:[
+    new Dotenv(),
+  ]
 };
