5ac5e21329da70f0bb0ff80835b8c70ace87b9dc
masato-210831 <b86500b@gmail.com>
2024-07-04 10:12:08 +0900

    start TypeScript セクション13

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,24 @@
-const path = require('path') // requireはnode.jsでモジュールをインストールする構文
+const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/app.ts', // ブラウザで初めに読み込まれるモジュールを指定、ルートノード？？
+  entry: './src/app.ts',
   output: {
-    filename: 'bundle.js', // 任意の名前でOK!!
-    path: path.resolve(__dirname, 'dist'), // 絶対pathなので注意！！
-    publicPath: '/dist' // dev-serverからの相対パス、紐付け
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist',
   },
   devtool: 'inline-source-map',
-  devServer: {
-    static:[
-      {
-        directory: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist',
-      },
-      {
-        directory: __dirname,
-        publicPath: '/',
-      },
-    ],
-  },
-  devtool: 'eval',
-  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'] // import文からtsとjs拡張子のファイルを探す
-  }
+    extensions: ['.ts', '.js'],
+  },
 };
